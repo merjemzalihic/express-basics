167c57a7e385fcafb186c57b38233261c1c29671
Merjem Zalihić <merjemzalihic.dev@outlook.com>
2023-12-06 18:53:29 +0100

    Added prisma client

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
+const prisma = new PrismaClient();
+
+// app.get("/", async (req, res) => {
+//   const data = await prisma.user.findMany();
+//   res.send(data);
+// });
 
 app.listen(8080, () => {
   console.log("Server is running at http://localhost:8080");
